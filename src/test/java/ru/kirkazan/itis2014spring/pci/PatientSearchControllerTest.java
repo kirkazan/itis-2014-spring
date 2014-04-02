c16b6cdedf717f50fcce3b0286b16cd83f5ce8c0
Ольга <telejnikova.olga@gmail.com>
2014-04-02 15:30:13 +0400

    Change controllers

--- src/test/java/ru/kirkazan/itis2014spring/pci/PatientSearchControllerTest.java
+++ src/test/java/ru/kirkazan/itis2014spring/pci/PatientSearchControllerTest.java
@@ -19,20 +19,27 @@ public class PatientSearchControllerTest
     @Mock
     PatientService patientService;
 
+//    @Test
+//    public void test()
+//    {
+//        patientController.search("СЭР82");
+//
+//        Mockito.verify(patientService).searchByFioAndBd("С","Э","Р",82);
+//    }
+//
+//    @Test
+//    public void test2() {
+//
+//        patientController.search("Садыков Эдуард Рустемович");
+//
+//        Mockito.verify(patientService).searchByName("Садыков", "Эдуард", "Рустемович");
+//    }
+
     @Test
-    public void test()
+    public void testCtrlFIOBD()
     {
         patientController.search("СЭР82");
 
         Mockito.verify(patientService).searchByFioAndBd("С","Э","Р",82);
-    }
-
-    @Test
-    public void test2() {
-
-        patientController.search("Садыков Эдуард Рустемович");
-
-        Mockito.verify(patientService).searchByName("Садыков", "Эдуард", "Рустемович");
-
     }
 }
